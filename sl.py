0f83a86974044aeb2caa78e3b658629f375b9143
Emre Tarakci <tarakci.software@gmail.com>
2023-07-25 10:41:24 +0300

    Add sample website test on python.com search bar

--- sl.py
+++ sl.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
-# from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 service = FirefoxService(executable_path="./geckodriver")
 driver = webdriver.Firefox(service=service)
 
-driver.get("https://dev.to")
+driver.get("https://www.python.org")
+print(driver.title)
+search_bar = driver.find_element(By.NAME, "q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
